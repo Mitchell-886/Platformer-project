39f35f33da8e12eb61d3b38b1361cac4b3dc92e0
Noah Mitchell <21.noah.mitchell@ksd.org>
2020-01-07 08:43:31 -0800

    Fix

--- Platformer/Platformer group/Assets/Scripts/PlayerHP.cs
+++ Platformer/Platformer group/Assets/Scripts/PlayerHP.cs
@@ -12,24 +12,24 @@ public class PlayerHP : MonoBehaviour
     public Slider Healthslider;
     public int lives = 0;
 
-    public void SavePlayer ()
-    {
-        SaveSystem.SavePlayer(this);
-    }
+    //public void SavePlayer ()
+    //{
+    //    SaveSystem.SavePlayer(this);
+   // }
 
-    public void LoadPlayer ()
-    {
-        PlayerData data = SaveSystem.LoadPlayer();
+    //public void LoadPlayer ()
+   // {
+    //    PlayerData data = SaveSystem.LoadPlayer();
 
         //level = data.Level;
-        health = data.Health;
+    //    health = data.Health;
 
-        Vector3 position;
-        position.x = data.position[0];
-        position.y = data.position[1];
-        position.z = data.position[2];
-        transform.position = position;
-    }
+   //     Vector3 position;
+   //     position.x = data.position[0];
+   //     position.y = data.position[1];
+   //     position.z = data.position[2];
+   //     transform.position = position;
+   // }
 
     private void Start()
     {
